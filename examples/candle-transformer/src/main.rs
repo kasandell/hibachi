75dd08792e3d481e8b8a3912331b85f42b223993
Kyle Sandell <kasandell@gmail.com>
2025-04-12 12:15:40 -0400

    adjust candle full model example

--- examples/candle-transformer/src/main.rs
+++ examples/candle-transformer/src/main.rs
@@ -5,10 +5,10 @@ use std::io;
 use std::io::{stdout, Write};
 use std::sync::Arc;
 use candle_core::{Tensor, Device, DType};
-use hibachi::BatchedRegressiveInference;
-use hibachi::Autoregressive;
+use hibachi_core::BatchedRegressiveInference;
+use hibachi_core::Autoregressive;
 use futures::stream::StreamExt;
-use hibachi::AutoregressiveBatcher;
+use hibachi_core::AutoregressiveBatcher;
 use tokenizers::Tokenizer;
 use crate::model::Model;
 use crate::token_output_stream::TokenOutputStream;
@@ -18,14 +18,15 @@ type Tensor2D = Tensor;
 
 #[tokio::main]
 async fn main() {
-    let model_pre = Model::new(Some(0.2), Some(25), Some(0.7));
-    let stop_token = model_pre.stop_token();
-    let tokenizer = model_pre.tokenizer();
-    let model = Box::new(model_pre);
+    let model = Model::new(Some(0.2), Some(25), Some(0.7));
+    let stop_token = model.stop_token();
+    let padding_token = model.padding_token();
+    let tokenizer = model.tokenizer();
 
-    let bi = Arc::new(BatchedRegressiveInference::<2>::new(
+    let bi = Arc::new(BatchedRegressiveInference::<Tensor, Model, 2>::new(
         model,
-        stop_token,
+        &stop_token,
+        &padding_token,
     ));
 
     let handles = (0..2).map(|e| {
@@ -40,12 +41,11 @@ async fn main() {
                     .get_ids()
                     .to_vec();
                 let toks = Tensor::new(sentence, &device).unwrap();//?.unsqueeze(0)?;
-                println!("starting toks: {:?}", toks);
                 let mut it = bic.clone().run(toks).await;
                 let mut count = 0;
                 let mut output = vec![];
                 while let Some(tok) = it.next().await {
-                    let value = tok.to_vec1::<u32>().expect("ok")[0];
+                    let value = tok.to_scalar::<u32>().expect("ok");
                     //println!("Index {} tok {}", e, value);
                     if let Some(out) = token_stream.next_token(value).unwrap() {
                         print!("{}", tc.id_to_token(value).unwrap().replace("Ġ", " "));
@@ -54,10 +54,6 @@ async fn main() {
                     output.push(value);
                     count+=1;
                 }
-                println!();
-                //println!("{}",tc.decode(&*output, true).unwrap());
-                println!("Index {} count {}", e, count);
-                println!();
             }.await
         })
     }).collect::<Vec<_>>();  // Collect into Vec to avoid lazy evaluation
