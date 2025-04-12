4e2e249aa48eadbb81a143762304a5a71b0620d7
Kyle Sandell <kasandell@gmail.com>
2025-04-12 15:52:32 -0400

    fix traits and examples

--- examples/candle-example/src/model.rs
+++ examples/candle-example/src/model.rs
@@ -1,4 +1,4 @@
-use hibachi::{Autoregressive};
+use hibachi::autoregressive::{Autoregressive};
 use async_trait::async_trait;
 use rand::{thread_rng, Rng};
 use candle_core::{Tensor};
@@ -13,15 +13,13 @@ impl Model {
 }
 
 #[async_trait]
-impl Autoregressive for Model {
-    type Sequence = Tensor;
-    type Output = Tensor;
+impl Autoregressive<Tensor> for Model {
 
-    async fn forward(&self, tensor: Self::Sequence) -> Self::Output {
+    async fn forward(&self, tensor: Tensor) -> Tensor {
         // Extract the dimensions we need
         let batch_size = tensor.dims()[0];
         let mut rng = thread_rng();
-        let val = rng.gen_range(0..batch_size/10);
+        let val = rng.gen_range(0..=batch_size);
         let mut zeros = Tensor::zeros(&[batch_size], tensor.dtype(), &tensor.device()).unwrap();
         let ones = Tensor::ones(&[1], tensor.dtype(), &tensor.device()).unwrap();
         for _ in 0..val {
@@ -29,5 +27,17 @@ impl Autoregressive for Model {
             zeros = zeros.slice_assign(&[idx..idx+1], &ones).unwrap();
         }
         zeros
+        /*
+        let mut rng = thread_rng();
+        let mut shape = tensor.dims().to_vec();
+        shape.remove(1);
+
+        if rng.gen_range(0..1000) < 5 {
+            return Tensor::ones(shape, tensor.dtype(), &tensor.device()).unwrap();
+        }
+        let zeros = Tensor::zeros(shape, tensor.dtype(), &tensor.device()).unwrap();
+        zeros
+
+         */
     }
 }
